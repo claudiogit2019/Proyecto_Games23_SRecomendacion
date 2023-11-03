82536b830cefec6faeaeb6f1c8fd19dfddc33b9a
claudiogit2019 <claudiounju2012@gmail.com>
2023-11-02 23:09:47 -0300

    test2

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,50 +1,46 @@
+from fastapi import FastAPI
 import pandas as pd
-from fastapi import FastAPI, HTTPException
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from datetime import datetime
-
-#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 app = FastAPI()
 
-
-#///////////////////////////////////////////////////////////////////////////////////////////////////
+# Cargar el DataFrame
 df_juegos = pd.read_csv('dfx_merge_sistema.csv')
 
-    # Combinar 
-df_juegos['combined_features'] = df_juegos['Genero'] + ' ' + df_juegos['Etiquetas'] + ' ' + df_juegos['Especificaciones'] 
+# Combinar las características relevantes
+df_juegos['combined_features'] = df_juegos['Genero'] + ' ' + df_juegos['Etiquetas'] + ' ' + df_juegos['Especificaciones']
 
-    # Asegurar
-df_juegos['combined_features'] = df_juegos['combined_features'].apply(lambda x: ' '.join(x.split()))
+# Inicializar el vectorizador TF-IDF
+tfidf_vectorizer = TfidfVectorizer()
 
-    # Matriz
-count_vectorizer = CountVectorizer()
-count_matrix = count_vectorizer.fit_transform(df_juegos['combined_features'])
+# Calcular la matriz TF-IDF
+tfidf_matrix = tfidf_vectorizer.fit_transform(df_juegos['combined_features'])
 
-    # Similitud 
-cosine_sim = cosine_similarity(count_matrix, count_matrix)
+# Calcular la similitud coseno
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 @app.get('/recomendacion_juego/{Id}')
-
 def recomendacion_juego(Id: int):
     try:
-        
         idx = df_juegos[df_juegos['Id'] == Id].index[0]
-
-        
         sim_scores = list(enumerate(cosine_sim[idx]))
-
         
+        # Ordenar por similitud y excluir el juego original
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = [x for x in sim_scores if x[0] != idx]
 
-        
-        top_juegos = sim_scores[1:6]
+        # Elegir las primeras 5 recomendaciones, evitando duplicados
+        recommended_juegos = []
+        seen_ids = set()
+        for i in sim_scores:
+            juego_id = i[0]
+            if juego_id not in seen_ids and i[1] < 0.4:  # Establece un umbral de similitud
+                recommended_juegos.append(df_juegos.iloc[juego_id]['Nombre_del_contenido'])
+                seen_ids.add(juego_id)
+            if len(recommended_juegos) >= 5:
+                break
 
-        
-        recommended_juegos = [df_juegos.iloc[i[0]]['Nombre_del_contenido'] for i in top_juegos]
-
-        return recommended_juegos
+        return recommended_juegos[:5]  # Limita el resultado a 5 recomendaciones
     except IndexError:
         return {"message": "Juego no encontrado"}
